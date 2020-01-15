1c6daca1bc23de2b3ead2f4e71c0ba8a9165b18c
Alina <linakuptsova92@gmail.com>
2020-01-15 17:02:11 +0300

    feat: add gallery section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,301 @@
 if(!window.jQuery){
 document.write('<script type="text/javascript" src="/js/lib/jquery.js"></script>')
 }
+
+$(document).ready(function () {
+ 
+  var modal = $('.modal');
+  var modalUp = $('.modal-up');
+  
+  var modalForm = $('.modal__form');
+  var heroForm = $('.hero__form');
+  var offerForm = $('.offer__form');
+  
+  onSubmitForm(modalForm)
+  onSubmitForm(heroForm)
+  onSubmitForm(offerForm)
+
+  $('[data-toggle=modal]').on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  $('.modal__close').on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  $('.modal-up__close').on('click', function () {
+    modalUp.removeClass('modal-up--visible');
+  });
+
+  $(document).keydown(function () {
+    if (event.keyCode == 27) {
+      modal.removeClass('modal--visible');
+      modalUp.removeClass('modal-up--visible');
+    }
+  });
+
+  // slider
+   //Initialize Swiper
+    var mySwiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      loop: true,
+      effect: 'fade',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  
+  new WOW().init();
+
+  modalForm.validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+
+      userName: {
+        required: true,
+        minlength: 2
+      },
+
+      userPhone: {
+        required: true,
+        minlength: 17
+      },
+ 
+      userEmail: {
+        required: true,
+        email: true
+      },
+    }, 
+
+    messages: {
+      userName: {
+        required: "Имя обязательно для заполнения",
+        minlength: "Имя не короче 2-х букв"
+      },
+      userPhone: "Телефон обязателен для заполнения",
+      userEmail: {
+        required: "Обязательно укажите Email",
+        email: "Введите в формате: name@domain.com"
+      },
+      policyCheckbox: "Обязательно для заполнения",
+    }
+  });
+
+  function onSubmitForm(form) { 
+   form.submit(function (event) {
+     if (form.valid()) {
+       event.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "send.php",
+          data: $(this).serialize(),
+          success: function (response) {
+           form[0].reset();
+           $('.modal').removeClass('modal--visible'); 
+           $('.modal-up').addClass('modal-up--visible');
+            console.log(response)
+        },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.error(jqXHR + " " + textStatus);
+            
+       }
+       });
+  }});
+  }
+
+  heroForm.validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: {
+        required: true,
+        minlength: 17
+      },
+  }, 
+  messages: {
+    userName: {
+      required: "Имя обязательно для заполнения",
+      minlength: "Имя не короче 2-х букв"
+    },
+    userPhone: "Телефон обязателен для заполнения",
+  }
+  });
+
+   footerForm.validate({
+    errorClass: "invalid",
+     errorElement: "div",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: {
+        required: true,
+        minlength: 17
+      },
+      userQuestion: "required",
+  }, 
+  messages: {
+   userName: {
+      required: "Имя обязательно для заполнения",
+      minlength: "Имя не короче 2-х букв"
+    },
+    userPhone: "Телефон обязателен для заполнения",
+    userQuestion: "Пожалуйста, напишите Ваш вопрос",
+  }
+  });
+  
+  $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7(___) ___-__-__" });
+
+  var player;
+  $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '360',
+      width: '100%',
+      videoId: 'W25aL7SWeJk',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event) {
+    event.target.playVideo();
+  }
+});
+
+//Переменная для включения/отключения индикатора загрузки
+var spinner = $('.ymap-container').children('.loader');
+//Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
+var check_if_load = false;
+//Необходимые переменные для того, чтобы задать координаты на Яндекс.Карте
+var myMapTemp, myPlacemarkTemp;
+ 
+//Функция создания карты сайта и затем вставки ее в блок с идентификатором &#34;map-yandex&#34;
+function init () {
+  var myMapTemp = new ymaps.Map('map', {
+    center: [47.244734, 39.723227],
+    zoom: 18
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+    
+   myPlacemark = new ymaps.Placemark(myMapTemp.getCenter(), {
+      hintContent: 'Наш офис',
+      balloonContent: 'Вход со двора'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/location-pin.png',
+      // Размеры метки.
+      iconImageSize: [32, 32],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    });
+
+  myMapTemp.geoObjects.add(myPlacemark); // помещаем флажок на карту
+  myMapTemp.behaviors.disable('scrollZoom'); 
+ 
+  // Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+  var layer = myMapTemp.layers.get(0).get(0);
+ 
+  // Решение по callback-у для определения полной загрузки карты
+  waitForTilesLoad(layer).then(function() {
+    // Скрываем индикатор загрузки после полной загрузки карты
+    spinner.removeClass('is-active');
+  });
+}
+ 
+// Функция для определения полной загрузки карты (на самом деле проверяется загрузка тайлов) 
+function waitForTilesLoad(layer) {
+  return new ymaps.vow.Promise(function (resolve, reject) {
+    var tc = getTileContainer(layer), readyAll = true;
+    tc.tiles.each(function (tile, number) {
+      if (!tile.isReady()) {
+        readyAll = false;
+      }
+    });
+    if (readyAll) {
+      resolve();
+    } else {
+      tc.events.once("ready", function() {
+        resolve();
+      });
+    }
+  });
+}
+ 
+function getTileContainer(layer) {
+  for (var k in layer) {
+    if (layer.hasOwnProperty(k)) {
+      if (
+        layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+        || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+      ) {
+        return layer[k];
+      }
+    }
+  }
+  return null;
+}
+// Функция загрузки API Яндекс.Карт по требованию (в нашем случае при наведении)
+function loadScript(url, callback){
+  var script = document.createElement("script");
+ 
+  if (script.readyState){  // IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+              script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  // Другие браузеры
+    script.onload = function(){
+      callback();
+    };
+  }
+ 
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+} 
+// Основная функция, которая проверяет когда мы навели на блок с классом &#34;ymap-container&#34;
+var ymap = function() {
+  $('.ymap-container').mouseenter(function(){
+      if (!check_if_load) { // проверяем первый ли раз загружается Яндекс.Карта, если да, то загружаем
+ 
+	  	// Чтобы не было повторной загрузки карты, мы изменяем значение переменной
+        check_if_load = true; 
+ 
+		// Показываем индикатор загрузки до тех пор, пока карта не загрузится
+        spinner.addClass('is-active');
+ 
+		// Загружаем API Яндекс.Карт
+        loadScript("https://api-maps.yandex.ru/2.1/?apikey=5638cd1f-e2e5-444a-8a22-2b670e272292&lang=ru_RU", function(){
+           // Как только API Яндекс.Карт загрузились, сразу формируем карту и помещаем в блок с идентификатором &#34;map-yandex&#34;
+           ymaps.load(init);
+        });                
+      }
+    }
+  );  
+}
+ 
+$(function() {
+ 
+  //Запускаем основную функцию
+  ymap();
+ 
+});
