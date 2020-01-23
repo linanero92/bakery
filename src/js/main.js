efa6eb59a19968783175b31234859d936e0202fe
Alina <linakuptsova92@gmail.com>
2020-01-23 17:35:43 +0300

    feat: add photo and footer

--- src/js/main.js
+++ src/js/main.js
@@ -134,7 +134,7 @@ $(document).ready(function () {
         minlength: 17
       },
       userEmail: {
-        required: "Обязательно укажите Email",
+        required: true,
         email: "Введите в формате: name@domain.com"
       },
   }, 
@@ -171,19 +171,34 @@ $(document).ready(function () {
   
   // slider video
    //Initialize Swiper
-    var mySwiper = new Swiper('.swiper-container--video', {
-      spaceBetween: 30,
-      loop: true,
-      effect: 'fade',
-      pagination: {
-        el: '.swiper-pagination--video',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next--video',
-        prevEl: '.swiper-button-prev--video',
-      },
-    });
+    // var mySwiper = new Swiper('.swiper-container--video', {
+    //   spaceBetween: 30,
+    //   loop: true,
+    //   effect: 'fade',
+    //   pagination: {
+    //     el: '.swiper-pagination--video',
+    //     clickable: true,
+    //   },
+    //   navigation: {
+    //     nextEl: '.swiper-button-next--video',
+    //     prevEl: '.swiper-button-prev--video',
+    //   },
+    // });
+  
+    var mySwiper1 = new Swiper ('.swiper-container-about', {
+    noSwiping:true,
+    loop:true,
+    pagination: {
+      el: '.swiper-pagination-about',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-about',
+      prevEl: '.swiper-button-prev-about',
+    },
+    
+  })
   
   var player;
   $('.video__play').on('click', function onYouTubeIframeAPIReady() {
@@ -252,5 +267,47 @@ function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
               this.parentNode.replaceChild(iframe, this);
           }
       }
+    });
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+  function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+    r(function(){
+      if (!document.getElementsByClassName) {
+          // Поддержка IE8
+          var getElementsByClassName = function(node, classname) {
+              var a = [];
+              var re = new RegExp('(^| )'+classname+'( |$)');
+              var els = node.getElementsByTagName("*");
+              for(var i=0,j=els.length; i < j; i++)
+                  if(re.test(els[i].className))a.push(els[i]);
+              return a;
+          }
+          var videos = getElementsByClassName(document.body,"youtube");
+      } else {
+          var videos = document.getElementsByClassName("youtube");
+      }
+      var nb_videos = videos.length;
+      for (var i=0; i < nb_videos; i++) {
+          // Находим постер для видео, зная ID нашего видео
+          videos[i].style.backgroundImage = 'url(../img/about/preload.jpg)';
+          // Размещаем над постером кнопку Play, чтобы создать эффект плеера
+          var play = document.createElement("div");
+          play.setAttribute("class","play");
+          videos[i].appendChild(play);
+          videos[i].onclick = function() {
+              // Создаем iFrame и сразу начинаем проигрывать видео, т.е. атрибут autoplay у видео в значении 1
+              var iframe = document.createElement("iframe");
+              var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+              if (this.getAttribute("data-params")) iframe_url+='&'+this.getAttribute("data-params");
+              iframe.setAttribute("src",iframe_url);
+              iframe.setAttribute("frameborder",'0');
+              // Высота и ширина iFrame будет как у элемента-родителя
+              iframe.style.width  = this.style.width;
+              iframe.style.height = this.style.height;
+              // Заменяем начальное изображение (постер) на iFrame
+              this.parentNode.replaceChild(iframe, this);
+          }
+      }
+    });
   });
 });
